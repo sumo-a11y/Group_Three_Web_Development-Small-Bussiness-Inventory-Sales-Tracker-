9548eb7b0e85e038e1b627cc1a7b5fac98630e9d
Rolandzogbay <rzogbay@gmail.com>
2026-02-23 16:58:00 +0000

    Hero section update

---
+++ frontend/src/components/heroButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="bg-orange-600 text-white font-bold py-2 px-4 rounded hover:bg-orange-700 transition"
+  >
+    <slot></slot>
+  </button>
+</template>
--- frontend/src/pages/LandingPage.vue
+++ frontend/src/pages/LandingPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <NavBar />
-    <div class="flex flex-col items-center justify-center h-100">
+    <div class="flex flex-col items-center justify-center h-150">
       <h1 class="text-5xl font-bold text-center mt-20">Inventory Management</h1>
       <span class="text-orange-600 mt-2 text-5xl font-bold">Made Simple</span>
       <p class="text-center mt-4 text-lg text-gray-600 max-w-2xl">
@@ -9,9 +9,18 @@
         with our intuitive platform. Track everything from stock levels to sales
         performance in one place.
       </p>
+      <div class="flex gap-4">
+        <HeroButton class="mt-6"
+          >Start your free trial <i class="fa-solid fa-arrow-right"></i
+        ></HeroButton>
+        <HeroButton class="mt-4"
+          >Sign In <i class="fa-solid fa-arrow-right"></i
+        ></HeroButton>
+      </div>
     </div>
   </div>
 </template>
 <script setup>
 import NavBar from "@/components/NavBar.vue";
+import HeroButton from "@/components/heroButton.vue";
 </script>
