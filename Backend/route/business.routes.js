1ee72c7d96b0957de2874849e0dcaf5d5746dabb
Rolandzogbay <rzogbay@gmail.com>
2026-03-02 16:18:33 +0000

    Fix merge issue

--- Backend/route/business.routes.js
+++ Backend/route/business.routes.js
@@ -16,13 +16,13 @@ import { roleMiddleware } from "../utils/middlewares/roleMiddleware.js";
 
 const businessRoutes = express.Router();
 
-businessRoutes.post("/", authMiddleware, roleMiddleware(["system_admin"]), createBusiness);
-businessRoutes.get("/", authMiddleware, roleMiddleware(["system_admin"]), getAllBusinesses);
+businessRoutes.post("/", authMiddleware, roleMiddleware("system_admin"), createBusiness);
+businessRoutes.get("/", authMiddleware, roleMiddleware("system_admin"), getAllBusinesses);
 
-businessRoutes.get("/search", authMiddleware, roleMiddleware(['system_admin']), searchBusinesses);
-businessRoutes.get("/paginated", authMiddleware, roleMiddleware(["system_admin"]), getBusinessesPaginated);
-businessRoutes.put("/:id", authMiddleware, roleMiddleware(["system_admin"]), updateBusiness);
-businessRoutes.get("/:id/users", authMiddleware, roleMiddleware(["system_admin", "business_admin"]), addOns.getBusinessUsers);
-businessRoutes.delete("/:id", authMiddleware, roleMiddleware(["system_admin"]), deleteBusiness);
+businessRoutes.get("/search", authMiddleware, roleMiddleware('system_admin'), searchBusinesses);
+businessRoutes.get("/paginated", authMiddleware, roleMiddleware("system_admin"), getBusinessesPaginated);
+businessRoutes.put("/:id", authMiddleware, roleMiddleware("system_admin"), updateBusiness);
+businessRoutes.get("/:id/users", authMiddleware, roleMiddleware("system_admin", "business_admin"), addOns.getBusinessUsers);
+businessRoutes.delete("/:id", authMiddleware, roleMiddleware("system_admin"), deleteBusiness);
 
 export default businessRoutes;
